tomscii/zutty

commit 15100bb5da8b26cb29c0137db2c0d946763535b9

Correct keypad mappings, simplify code, improve documentation
Closes #81

src/vterm.cc
@@ -153,19 +153,19 @@ namespace
       {Key::Backspace,   "\x7f"},
       {Key::Tab,         "\t"},
       {Key::Return,      "\r"},
-      {Key::Insert,      CSI "2~"},    {Key::KP_Insert,   CSI "2~"},
-      {Key::Delete,      CSI "3~"},    {Key::KP_Delete,   CSI "3~"},
-      {Key::PageUp,      CSI "5~"},    {Key::KP_PageUp,   CSI "5~"},
-      {Key::PageDown,    CSI "6~"},    {Key::KP_PageDown, CSI "6~"},
+      {Key::Insert,      CSI "2~"},
+      {Key::Delete,      CSI "3~"},
+      {Key::PageUp,      CSI "5~"},
+      {Key::PageDown,    CSI "6~"},
       {Key::NONE,        nullptr},
    };
 
    const InputSpec is_Mod_Ansi [] =
    {
-      {Key::Insert,      CSI "2;" MC "~"},  {Key::KP_Insert,   CSI "2;" MC "~"},
-      {Key::Delete,      CSI "3;" MC "~"},  {Key::KP_Delete,   CSI "3;" MC "~"},
-      {Key::PageUp,      CSI "5;" MC "~"},  {Key::KP_PageUp,   CSI "5;" MC "~"},
-      {Key::PageDown,    CSI "6;" MC "~"},  {Key::KP_PageDown, CSI "6;" MC "~"},
+      {Key::Insert,      CSI "2;" MC "~"},
+      {Key::Delete,      CSI "3;" MC "~"},
+      {Key::PageUp,      CSI "5;" MC "~"},
+      {Key::PageDown,    CSI "6;" MC "~"},
       {Key::NONE,        nullptr},
    };
 
@@ -228,18 +228,18 @@ namespace
       {Key::KP_Plus,     "+"},
       {Key::KP_Comma,    ","},
       {Key::KP_Minus,    "-"},
-      {Key::KP_Dot,      "."},
       {Key::KP_Slash,    "/"},
-      {Key::KP_0,        "0"},
-      {Key::KP_1,        "1"},
-      {Key::KP_2,        "2"},
-      {Key::KP_3,        "3"},
-      {Key::KP_4,        "4"},
-      {Key::KP_5,        "5"},
-      {Key::KP_6,        "6"},
-      {Key::KP_7,        "7"},
-      {Key::KP_8,        "8"},
-      {Key::KP_9,        "9"},
+      {Key::KP_Delete,   "."},  {Key::KP_Dot,      "."},
+      {Key::KP_Insert,   "0"},  {Key::KP_0,        "0"},
+      {Key::KP_End,      "1"},  {Key::KP_1,        "1"},
+      {Key::KP_Down,     "2"},  {Key::KP_2,        "2"},
+      {Key::KP_PageDown, "3"},  {Key::KP_3,        "3"},
+      {Key::KP_Left,     "4"},  {Key::KP_4,        "4"},
+      {Key::KP_Begin,    "5"},  {Key::KP_5,        "5"},
+      {Key::KP_Right,    "6"},  {Key::KP_6,        "6"},
+      {Key::KP_Home,     "7"},  {Key::KP_7,        "7"},
+      {Key::KP_Up,       "8"},  {Key::KP_8,        "8"},
+      {Key::KP_PageUp,   "9"},  {Key::KP_9,        "9"},
       {Key::KP_Equal,    "="},
       {Key::NONE,        nullptr},
    };
@@ -278,18 +278,18 @@ namespace
       {Key::KP_Plus,     SS3 MC "k"},
       {Key::KP_Comma,    SS3 MC "l"},
       {Key::KP_Minus,    SS3 MC "m"},
-      {Key::KP_Dot,      SS3 MC "n"},
+      {Key::KP_Delete,   SS3 MC "n"},
       {Key::KP_Slash,    SS3 MC "o"},
-      {Key::KP_0,        SS3 MC "p"},
-      {Key::KP_1,        SS3 MC "q"},
-      {Key::KP_2,        SS3 MC "r"},
-      {Key::KP_3,        SS3 MC "s"},
-      {Key::KP_4,        SS3 MC "t"},
-      {Key::KP_5,        SS3 MC "u"},
-      {Key::KP_6,        SS3 MC "v"},
-      {Key::KP_7,        SS3 MC "w"},
-      {Key::KP_8,        SS3 MC "x"},
-      {Key::KP_9,        SS3 MC "y"},
+      {Key::KP_Insert,   SS3 MC "p"},
+      {Key::KP_End,      SS3 MC "q"},
+      {Key::KP_Down,     SS3 MC "r"},
+      {Key::KP_PageDown, SS3 MC "s"},
+      {Key::KP_Left,     SS3 MC "t"},
+      {Key::KP_Begin,    SS3 MC "u"},
+      {Key::KP_Right,    SS3 MC "v"},
+      {Key::KP_Home,     SS3 MC "w"},
+      {Key::KP_Up,       SS3 MC "x"},
+      {Key::KP_PageUp,   SS3 MC "y"},
       {Key::KP_Equal,    SS3 MC "X"},
       {Key::NONE,        nullptr},
    };
@@ -303,18 +303,18 @@ namespace
       {Key::KP_Plus,     ESC "?k"},
       {Key::KP_Comma,    ESC "?l"},
       {Key::KP_Minus,    ESC "?m"},
-      {Key::KP_Dot,      ESC "?n"},
+      {Key::KP_Delete,   ESC "?n"},
       {Key::KP_Slash,    ESC "?o"},
-      {Key::KP_0,        ESC "?p"},
-      {Key::KP_1,        ESC "?q"},
-      {Key::KP_2,        ESC "?r"},
-      {Key::KP_3,        ESC "?s"},
-      {Key::KP_4,        ESC "?t"},
-      {Key::KP_5,        ESC "?u"},
-      {Key::KP_6,        ESC "?v"},
-      {Key::KP_7,        ESC "?w"},
-      {Key::KP_8,        ESC "?x"},
-      {Key::KP_9,        ESC "?y"},
+      {Key::KP_Insert,   ESC "?p"},
+      {Key::KP_End,      ESC "?q"},
+      {Key::KP_Down,     ESC "?r"},
+      {Key::KP_PageDown, ESC "?s"},
+      {Key::KP_Left,     ESC "?t"},
+      {Key::KP_Begin,    ESC "?u"},
+      {Key::KP_Right,    ESC "?v"},
+      {Key::KP_Home,     ESC "?w"},
+      {Key::KP_Up,       ESC "?x"},
+      {Key::KP_PageUp,   ESC "?y"},
       {Key::KP_Equal,    ESC "?X"},
       {Key::NONE,        nullptr},
    };
@@ -330,49 +330,45 @@ namespace
 
    const InputSpec is_Ansi_CursorKeys [] =
    {
-      {Key::Up,          CSI "A"},      {Key::KP_Up,       CSI "A"},
-      {Key::Down,        CSI "B"},      {Key::KP_Down,     CSI "B"},
-      {Key::Right,       CSI "C"},      {Key::KP_Right,    CSI "C"},
-      {Key::Left,        CSI "D"},      {Key::KP_Left,     CSI "D"},
-      /* TODO is there a Begin key? */  {Key::KP_Begin,    CSI "E"},
-      {Key::Home,        CSI "H"},      {Key::KP_Home,     CSI "H"},
-      {Key::End,         CSI "F"},      {Key::KP_End,      CSI "F"},
+      {Key::Up,          CSI "A"},
+      {Key::Down,        CSI "B"},
+      {Key::Right,       CSI "C"},
+      {Key::Left,        CSI "D"},
+      {Key::Home,        CSI "H"},
+      {Key::End,         CSI "F"},
       {Key::NONE,        nullptr},
    };
 
    const InputSpec is_Appl_CursorKeys [] =
    {
-      {Key::Up,          SS3 "A"},      {Key::KP_Up,       SS3 "A"},
-      {Key::Down,        SS3 "B"},      {Key::KP_Down,     SS3 "B"},
-      {Key::Right,       SS3 "C"},      {Key::KP_Right,    SS3 "C"},
-      {Key::Left,        SS3 "D"},      {Key::KP_Left,     SS3 "D"},
-      /* TODO is there a Begin key? */  {Key::KP_Begin,    SS3 "E"},
-      {Key::Home,        SS3 "H"},      {Key::KP_Home,     SS3 "H"},
-      {Key::End,         SS3 "F"},      {Key::KP_End,      SS3 "F"},
+      {Key::Up,          SS3 "A"},
+      {Key::Down,        SS3 "B"},
+      {Key::Right,       SS3 "C"},
+      {Key::Left,        SS3 "D"},
+      {Key::Home,        SS3 "H"},
+      {Key::End,         SS3 "F"},
       {Key::NONE,        nullptr},
    };
 
    const InputSpec is_Mod_CursorKeys [] =
    {
-      {Key::Up,          CSI "1;" MC "A"},   {Key::KP_Up,     CSI "1;" MC "A"},
-      {Key::Down,        CSI "1;" MC "B"},   {Key::KP_Down,   CSI "1;" MC "B"},
-      {Key::Right,       CSI "1;" MC "C"},   {Key::KP_Right,  CSI "1;" MC "C"},
-      {Key::Left,        CSI "1;" MC "D"},   {Key::KP_Left,   CSI "1;" MC "D"},
-      /* TODO is there a Begin key? */       {Key::KP_Begin,  CSI "1;" MC "E"},
-      {Key::Home,        CSI "1;" MC "H"},   {Key::KP_Home,   CSI "1;" MC "H"},
-      {Key::End,         CSI "1;" MC "F"},   {Key::KP_End,    CSI "1;" MC "F"},
+      {Key::Up,          CSI "1;" MC "A"},
+      {Key::Down,        CSI "1;" MC "B"},
+      {Key::Right,       CSI "1;" MC "C"},
+      {Key::Left,        CSI "1;" MC "D"},
+      {Key::Home,        CSI "1;" MC "H"},
+      {Key::End,         CSI "1;" MC "F"},
       {Key::NONE,        nullptr},
    };
 
    const InputSpec is_VT52_CursorKeys [] =
    {
-      {Key::Up,          ESC "A"},      {Key::KP_Up,       ESC "A"},
-      {Key::Down,        ESC "B"},      {Key::KP_Down,     ESC "B"},
-      {Key::Right,       ESC "C"},      {Key::KP_Right,    ESC "C"},
-      {Key::Left,        ESC "D"},      {Key::KP_Left,     ESC "D"},
-      /* TODO is there a Begin key? */  {Key::KP_Begin,    ESC "E"},
-      {Key::Home,        ESC "H"},      {Key::KP_Home,     ESC "H"},
-      {Key::End,         ESC "F"},      {Key::KP_End,      ESC "F"},
+      {Key::Up,          ESC "A"},
+      {Key::Down,        ESC "B"},
+      {Key::Right,       ESC "C"},
+      {Key::Left,        ESC "D"},
+      {Key::Home,        ESC "H"},
+      {Key::End,         ESC "F"},
       {Key::NONE,        nullptr},
    };
 
@@ -878,23 +874,17 @@ namespace zutty
            is_BackspaceKey_BkSp
          },
 
+         { [this] () { return (compatLevel == CompatibilityLevel::VT52 &&
+                               keypadMode == KeypadMode::Application); },
+           is_VT52_KeypadKeys
+         },
          { [this] () { return (compatLevel == CompatibilityLevel::VT52); },
            is_VT52_CursorKeys
          },
          { [this] () { return (compatLevel == CompatibilityLevel::VT52); },
            is_VT52_FunctionKeys
          },
-         { [this] () { return (compatLevel == CompatibilityLevel::VT52 &&
-                               keypadMode == KeypadMode::Application); },
-           is_VT52_KeypadKeys
-         },
 
-         { [this] () { return (modifiers != Mod::none); },
-           is_Mod_CursorKeys
-         },
-         { [this] () { return (cursorKeyMode == CursorKeyMode::Application); },
-           is_Appl_CursorKeys
-         },
          { [this] () { return (modifiers != Mod::none &&
                                keypadMode == KeypadMode::Application); },
            is_Mod_Appl_KeypadKeys
@@ -902,8 +892,14 @@ namespace zutty
          { [this] () { return (keypadMode == KeypadMode::Application); },
            is_Appl_KeypadKeys
          },
+         { [this] () { return (cursorKeyMode == CursorKeyMode::Application); },
+           is_Appl_CursorKeys
+         },
 
          // entries to use with modifier keys being held
+         { [this] () { return (modifiers != Mod::none); },
+           is_Mod_CursorKeys
+         },
          { [this] () { return (modifiers != Mod::none); },
            is_Mod_Ansi
          },

test/keys.sh
@@ -8,25 +8,20 @@ CHECK_DEPS setxkbmap
 function INS_DEL_PGUPDN {
     IN "printf \"\\\\e[H\\\\e[J\" && cat -vT\r"
 
-    IN "Testing Ins/Del/PageUp/PageDown in normal mode. \D1"
+    IN "Testing Ins/Del/PageUp/PageDown in normal mode. \n\D1"
     IN "No modifiers: \D1"
     IN "\[Insert] \[Delete] \[Page_Up] \[Page_Down] \D1"
-    IN "\[KP_Insert] \[KP_Delete] \[KP_Page_Up] \[KP_Page_Down] \D1"
 
     IN "Shift: \D1"
     # Exclude Shift-Insert b/c it is bound to "Paste"
     # Exclude Shift-PageUp and Shift-PageDown (bound to scroll up/down)
     IN "\S\[Delete] \D1"
-    # N.B.: Shift state is not set on XKeyPressedEvent for keypad keys
-    # (may be a xvkbd limitation), so omit testing those
 
     IN "Alt: \D1"
     IN "\A\[Insert] \A\[Delete] \A\[Page_Up] \A\[Page_Down] \D1"
-    IN "\A\[KP_Insert] \A\[KP_Delete] \A\[KP_Page_Up] \A\[KP_Page_Down] \D1"
 
     IN "Control: \D1"
     IN "\C\[Insert] \C\[Delete] \C\[Page_Up] \C\[Page_Down] \D1"
-    IN "\C\[KP_Insert] \C\[KP_Delete] \C\[KP_Page_Up] \C\[KP_Page_Down] \D1"
 
     IN "Alt-Shift: \D1"
     IN "\A\S\[Delete] \A\S\[Page_Up] \A\S\[Page_Down] \D1"
@@ -36,12 +31,11 @@ function INS_DEL_PGUPDN {
 
     IN "Control-Alt: \D1"
     IN "\C\A\[Insert] \C\A\[Delete] \C\A\[Page_Up] \C\A\[Page_Down] \D1"
-    IN "\C\A\[KP_Insert] \C\A\[KP_Delete] \C\A\[KP_Page_Up] \C\A\[KP_Page_Down] \D1"
 
     IN "Control-Alt-Shift: \D1"
     IN "\C\A\S\[Delete] \C\A\S\[Page_Up] \C\A\S\[Page_Down] \D1"
 
-    SNAP keys_01 9f9018f1f830f45f0850ddd01446c527
+    SNAP keys_01 778895af5c80b2a19acc2bb2b9b048b5
 
     IN "\Cd\Cd\D3"
 }
@@ -109,48 +103,88 @@ function CURSOR_KEYS {
 function KEYPAD_KEYS {
     IN "printf \"\\\\e[H\\\\e[J\" && cat -vT\r"
 
-    IN "Testing keypad keys in normal mode. \D1"
+    IN "Testing keypad keys in normal mode. \n\D1"
     IN "No modifiers: \D1"
-    # VT100 Numeric mode - send "NumLock on" keysyms
-    IN "\[KP_Enter] \[KP_Tab] \[KP_Space] \[KP_Multiply] \[KP_Divide] "
+    # VT100 Numeric mode - test both "Numlock on" and "NumLock off" keysyms
+    IN "\[KP_Tab] \[KP_Space] \[KP_Multiply] \[KP_Divide] "
     IN "\[KP_Add] \[KP_Subtract] \[KP_Separator] \[KP_Decimal] \[KP_Equal] "
     IN "\[KP_0] \[KP_1] \[KP_2] \[KP_3] \[KP_4] "
     IN "\[KP_5] \[KP_6] \[KP_7] \[KP_8] \[KP_9] \D2"
+    IN "\[KP_Insert] \[KP_End] \[KP_Down] \[KP_Page_Down] \[KP_Left] "
+    IN "\[KP_Begin] \[KP_Right] \[KP_Home] \[KP_Up] \[KP_Page_Up] "
+    IN "\[KP_Enter]\D2"
 
-    SNAP keys_04 472833543e22148464efaebc61283156
+    IN "Alt: \D1"
+    IN "\A\[KP_Tab] \A\[KP_Space] \A\[KP_Multiply] \A\[KP_Divide] "
+    IN "\A\[KP_Add] \A\[KP_Subtract] \A\[KP_Separator] \A\[KP_Decimal] \A\[KP_Equal] "
+    IN "\A\[KP_0] \A\[KP_1] \A\[KP_2] \A\[KP_3] \A\[KP_4] "
+    IN "\A\[KP_5] \A\[KP_6] \A\[KP_7] \A\[KP_8] \A\[KP_9] \D2"
+    IN "\A\[KP_Insert] \A\[KP_End] \A\[KP_Down] \A\[KP_Page_Down] \A\[KP_Left] "
+    IN "\A\[KP_Begin] \A\[KP_Right] \A\[KP_Home] \A\[KP_Up] \A\[KP_Page_Up] "
+    IN "\A\[KP_Enter]\D2"
+
+    IN "Control: \D1"
+    IN "\C\[KP_Tab] \C\[KP_Space] \C\[KP_Multiply] \C\[KP_Divide] "
+    IN "\C\[KP_Add] \C\[KP_Subtract] \C\[KP_Separator] \C\[KP_Decimal] \C\[KP_Equal] "
+    IN "\C\[KP_0] \C\[KP_1] \C\[KP_2] \C\[KP_3] \C\[KP_4] "
+    IN "\C\[KP_5] \C\[KP_6] \C\[KP_7] \C\[KP_8] \C\[KP_9] \D2"
+    IN "\C\[KP_Insert] \C\[KP_End] \C\[KP_Down] \C\[KP_Page_Down] \C\[KP_Left] "
+    IN "\C\[KP_Begin] \C\[KP_Right] \C\[KP_Home] \C\[KP_Up] \C\[KP_Page_Up] "
+    IN "\C\[KP_Enter]\D2"
+
+    IN "Control-Alt: \D1"
+    IN "\C\A\[KP_Tab] \C\A\[KP_Space] \C\A\[KP_Multiply] \C\A\[KP_Divide] "
+    IN "\C\A\[KP_Add] \C\A\[KP_Subtract] \C\A\[KP_Separator] \C\A\[KP_Decimal] \C\A\[KP_Equal] "
+    IN "\C\A\[KP_0] \C\A\[KP_1] \C\A\[KP_2] \C\A\[KP_3] \C\A\[KP_4] "
+    IN "\C\A\[KP_5] \C\A\[KP_6] \C\A\[KP_7] \C\A\[KP_8] \C\A\[KP_9] \D2"
+    IN "\C\A\[KP_Insert] \C\A\[KP_End] \C\A\[KP_Down] \C\A\[KP_Page_Down] \C\A\[KP_Left] "
+    IN "\C\A\[KP_Begin] \C\A\[KP_Right] \C\A\[KP_Home] \C\A\[KP_Up] \C\A\[KP_Page_Up] "
+    IN "\C\A\[KP_Enter] \D2"
+
+    SNAP keys_04 bb57a78dfa5d5b85ca9714ec5ddbf6cf
 
     IN "\Cd\Cd\D3"
     IN "printf \"\\\\e[H\\\\e[J\\\\e=\" && cat -vT\r"
 
-    IN "Testing keypad keys in application keypad mode. \D1"
+    IN "Testing keypad keys in application keypad mode. \n\D1"
     IN "No modifiers: \D1"
-    # VT100 Application mode - send "NumLock off" keysyms
-    IN "\[KP_Enter] \[KP_Tab] \[KP_Space] \[KP_Multiply] \[KP_Divide] "
-    IN "\[KP_Add] \[KP_Subtract] \[KP_Separator] \[KP_Delete] \[KP_Equal] "
+    # VT100 Application mode - test both "Numlock on" and "NumLock off" keysyms
+    IN "\[KP_Tab] \[KP_Space] \[KP_Multiply] \[KP_Divide] "
+    IN "\[KP_Add] \[KP_Subtract] \[KP_Separator] \[KP_Decimal] \[KP_Equal] "
+    IN "\[KP_0] \[KP_1] \[KP_2] \[KP_3] \[KP_4] "
+    IN "\[KP_5] \[KP_6] \[KP_7] \[KP_8] \[KP_9] \D2"
     IN "\[KP_Insert] \[KP_End] \[KP_Down] \[KP_Page_Down] \[KP_Left] "
-    IN "\[KP_Begin] \[KP_Right] \[KP_Home] \[KP_Up] \[KP_Page_Up] \D2"
-
-    # Omitting Shift as it does not get through for all keys (xvkbd?)
+    IN "\[KP_Begin] \[KP_Right] \[KP_Home] \[KP_Up] \[KP_Page_Up] "
+    IN "\[KP_Enter]    \D2"
 
     IN "Alt: \D1"
-    IN "\A\[KP_Enter] \A\[KP_Tab] \A\[KP_Space] \A\[KP_Multiply] \A\[KP_Divide] "
+    IN "\A\[KP_Tab] \A\[KP_Space] \A\[KP_Multiply] \A\[KP_Divide] "
     IN "\A\[KP_Add] \A\[KP_Subtract] \A\[KP_Separator] \A\[KP_Decimal] \A\[KP_Equal] "
     IN "\A\[KP_0] \A\[KP_1] \A\[KP_2] \A\[KP_3] \A\[KP_4] "
     IN "\A\[KP_5] \A\[KP_6] \A\[KP_7] \A\[KP_8] \A\[KP_9] \D2"
+    IN "\A\[KP_Insert] \A\[KP_End] \A\[KP_Down] \A\[KP_Page_Down] \A\[KP_Left] "
+    IN "\A\[KP_Begin] \A\[KP_Right] \A\[KP_Home] \A\[KP_Up] \A\[KP_Page_Up] "
+    IN "\A\[KP_Enter]    \D2"
 
     IN "Control: \D1"
-    IN "\C\[KP_Enter] \C\[KP_Tab] \C\[KP_Space] \C\[KP_Multiply] \C\[KP_Divide] "
+    IN "\C\[KP_Tab] \C\[KP_Space] \C\[KP_Multiply] \C\[KP_Divide] "
     IN "\C\[KP_Add] \C\[KP_Subtract] \C\[KP_Separator] \C\[KP_Decimal] \C\[KP_Equal] "
     IN "\C\[KP_0] \C\[KP_1] \C\[KP_2] \C\[KP_3] \C\[KP_4] "
     IN "\C\[KP_5] \C\[KP_6] \C\[KP_7] \C\[KP_8] \C\[KP_9] \D2"
+    IN "\C\[KP_Insert] \C\[KP_End] \C\[KP_Down] \C\[KP_Page_Down] \C\[KP_Left] "
+    IN "\C\[KP_Begin] \C\[KP_Right] \C\[KP_Home] \C\[KP_Up] \C\[KP_Page_Up] "
+    IN "\C\[KP_Enter]    \D2"
 
     IN "Control-Alt: \D1"
-    IN "\C\A\[KP_Enter] \C\A\[KP_Tab] \C\A\[KP_Space] "
-    IN "\C\A\[KP_Separator] \C\A\[KP_Decimal] \C\A\[KP_Equal] "
+    IN "\C\A\[KP_Tab] \C\A\[KP_Space] \C\A\[KP_Multiply] \C\A\[KP_Divide] "
+    IN "\C\A\[KP_Add] \C\A\[KP_Subtract] \C\A\[KP_Separator] \C\A\[KP_Decimal] \C\A\[KP_Equal] "
     IN "\C\A\[KP_0] \C\A\[KP_1] \C\A\[KP_2] \C\A\[KP_3] \C\A\[KP_4] "
     IN "\C\A\[KP_5] \C\A\[KP_6] \C\A\[KP_7] \C\A\[KP_8] \C\A\[KP_9] \D2"
+    IN "\C\A\[KP_Insert] \C\A\[KP_End] \C\A\[KP_Down] \C\A\[KP_Page_Down] \C\A\[KP_Left] "
+    IN "\C\A\[KP_Begin] \C\A\[KP_Right] \C\A\[KP_Home] \C\A\[KP_Up] \C\A\[KP_Page_Up] "
+    IN "\C\A\[KP_Enter]    \D2"
 
-    SNAP keys_05 ff17e245b367c04151b9a5bed35b30ae
+    SNAP keys_05 d54bdfb3f3b5773697b2cf3b7f8af1d1
 
     IN "\Cd\Cd\D3"
     IN "printf \"\\\\e>\"\r"

doc/KEYS.org
@@ -91,21 +91,20 @@ Modifier examples:
 - Shift + F1: CSI 1 ; 2 P
 - Alt + F4: CSI 1 ; 3 S
 - Shift + F5: CSI 1 5 ; 2 ~
-- Control + Shift + F10: CSI 21 ; 6 ~
+- Control + Shift + F10: CSI 2 1 ; 6 ~
 
 Modifiers are not applied in VT52 mode.
 
 * Sequences dependent on cursor key mode setting
 
-| Key             | VT100 Normal | VT100 App | VT52  |
-|-----------------+--------------+-----------+-------|
-| Up, KP_Up       | CSI A        | SS3 A     | ESC A |
-| Down, KP_Down   | CSI B        | SS3 B     | ESC B |
-| Right, KP_Right | CSI C        | SS3 C     | ESC C |
-| Left, KP_Left   | CSI D        | SS3 D     | ESC D |
-| KP_Begin        | CSI E        | SS3 E     | ESC E |
-| Home, KP_Home   | CSI H        | SS3 H     | ESC H |
-| End, KP_End     | CSI F        | SS3 F     | ESC F |
+| Key   | VT100 Normal | VT100 App | VT52  |
+|-------+--------------+-----------+-------|
+| Up    | CSI A        | SS3 A     | ESC A |
+| Down  | CSI B        | SS3 B     | ESC B |
+| Right | CSI C        | SS3 C     | ESC C |
+| Left  | CSI D        | SS3 D     | ESC D |
+| Home  | CSI H        | SS3 H     | ESC H |
+| End   | CSI F        | SS3 F     | ESC F |
 
 Examples (apply both in Normal and App mode):
 - Alt + Left: CSI 1 ; 3 D
@@ -113,17 +112,22 @@ Examples (apply both in Normal and App mode):
 
 Modifiers are not applied in VT52 mode.
 
+Note that arrow keys on the numeric keypad (=KP_Up=, etc.) are handled
+as completely different keys; their encoding method is described in
+the next section.
+
 * Sequences dependent on keypad mode setting
 
 This only affects the keypad. The corresponding non-keypad keys always
-send the characters in the Normal column. Normal (aka. numeric) mode
+send the literals in the Normal column. Normal (aka. numeric) mode
 is shared between VT100 and VT52.
 
 Some keys are received from X with different keysyms based on the
 NumLock state; for these keys, the second column shows the keysym with
-NumLock off.  With NumLock on, all keys generate their literals (as in
-normal mode).  With NumLock off, mappings are generated as below,
-depending on the terminal mode setting.
+NumLock off.  In VT100 App mode with NumLock on, all keys generate
+their literals (as in normal mode), but see further below regarding
+the Shift key.  With NumLock off, mappings are generated as per the
+below table, depending on the terminal mode setting.
 
 | NumLock on   | NumLock off  | Normal | VT100 App | VT52 App |
 |--------------+--------------+--------+-----------+----------|
@@ -148,18 +152,55 @@ depending on the terminal mode setting.
 | KP_9         | KP_Page_Up   | 9      | SS3 y     | ESC ? y  |
 | KP_Equal     |              | =      | SS3 X     | ESC ? X  |
 
-The intention is that Zutty behaves just like xterm with
-[[https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-VT220-Style-Function-Keys][VT220-style function keys]].
-
-In VT100 App mode, modifier keys do NOT cause the SS3 to change to
-CSI. This is how xterm behaves, and Zutty emulates that behaviour.
+Modifier keys do not cause the SS3 to change to CSI. This is how xterm
+behaves, and Zutty emulates that behaviour.
 
 Examples:
+- Alt + KP_Minus: SS3 3 m
 - Control + KP_Enter: SS3 5 M
 - Control + KP_Plus: SS3 5 k
-- Alt + KP_Minus: SS3 3 m
+- Control + Shift + KP_Left: SS3 6 t
 
 Modifiers are not applied in VT52 mode.
+
+There is some additional subtlety related to use of the Shift key.
+Pressing Shift has a momentary effect of inverting the NumLock state.
+This is not specific to Zutty; it works on the X level and is easily
+seen via e.g., =xev=. For example, with NumLock off, hitting "4" on
+the numeric keypad will generate =KP_Left=, while hitting the same key
+with Shift held down will generate =KP_4=. With NumLock on, results
+will be reversed: an unmodified keypress will generate =KP_4=, while a
+Shift-press will generate =KP_Left=.
+
+Why is this interesting? Let's go through the encodings of these keys.
+In normal (numeric) mode, Zutty generates the literals shown in the
+Normal column regardless of the NumLock state, and without any
+modifier encoding.  That is, hitting "4" on the keypad will yield a
+"4" with or without NumLock, and all modifiers will be discarded.
+
+In application (aka. ANSI, aka. alternate) keypad mode, however, all
+the details come into play.  Hitting keypad "4" will generate "4" only
+if NumLock is on; with NumLock off, it will be encoded as the escape
+sequence =SS3 t=.  Further, holding Shift while hitting the same key
+will have slightly asymmetrical results depending on NumLock state:
+
+- With NumLock off, Zutty will consider the keypress as a modifier
+  combination of Shift plus =KP_Left=, and encode it as =SS3 2 t=. In
+  the same manner, Control plus =KP_Left= will be encoded as =SS3 5
+  t=, and so on for other (possibly compounded) modifiers and keys.
+
+- With NumLock on, the baseline is to generate literals only, just as
+  in numeric mode. Modifiers will be ignored, so Control + =KP_4= (the
+  keysym Zutty sees due to NumLock being on) will still generate a "4"
+  only. However, if Shift is among the active modifiers, it will turn
+  =KP_4= back into =KP_Left=, and the result will be, again, encoded
+  as =SS3 2 t= (or the corresponding modifier code).
+
+The intention is that Zutty behaves just like xterm with [[https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-VT220-Style-Function-Keys][VT220-style
+function keys]]. In case of manual side-by-side comparisons, please
+remember to put xterm into "VT220 Keyboard" mode (by choosing it in
+the Control + left-click menu, setting the =XTerm.keyboardType= X
+resource to =vt220=, or passing =-kt vt220= on the command line).
 
 * Special key combinations
 

commit e1687aa2b104d9b3534b5f6610d6b87cc85e8683

Regression: Fix arrow key mappings in DECAKM (appl.cursor key) mode
Fix regression introduced in previous commit (15100bb).

src/vterm.cc
@@ -892,14 +892,13 @@ namespace zutty
          { [this] () { return (keypadMode == KeypadMode::Application); },
            is_Appl_KeypadKeys
          },
+         { [this] () { return (modifiers != Mod::none); },
+           is_Mod_CursorKeys
+         },
          { [this] () { return (cursorKeyMode == CursorKeyMode::Application); },
            is_Appl_CursorKeys
          },
 
-         // entries to use with modifier keys being held
-         { [this] () { return (modifiers != Mod::none); },
-           is_Mod_CursorKeys
-         },
          { [this] () { return (modifiers != Mod::none); },
            is_Mod_Ansi
          },

test/keys.sh
@@ -70,40 +70,71 @@ function FUNCTION_KEYS {
 function CURSOR_KEYS {
     IN "printf \"\\\\e[H\\\\e[J\" && cat -vT\r"
 
-    IN "Testing cursor keys in normal mode. \D1"
+    IN "Testing cursor keys in normal mode (DECCKM).\n\D1"
     IN "No modifiers: \D1"
-    IN "\[Up] \[Down] \[Right] \[Left] \[Home] \[End] \D2"
+    IN "\[Up] \[Down] \[Right] \[Left] \[Home] \[End] \n\D2"
 
     IN "Shift: \D1"
-    IN "\S\[Up] \S\[Down] \S\[Right] \S\[Left] \S\[Home] \S\[End] \D2"
+    IN "\S\[Up] \S\[Down] \S\[Right] \S\[Left] \S\[Home] \S\[End] \n\D2"
 
     IN "Alt: \D1"
-    IN "\A\[Up] \A\[Down] \A\[Right] \A\[Left] \A\[Home] \A\[End] \D2"
+    IN "\A\[Up] \A\[Down] \A\[Right] \A\[Left] \A\[Home] \A\[End] \n\D2"
 
     IN "Control: \D1"
-    IN "\C\[Up] \C\[Down] \C\[Right] \C\[Left] \C\[Home] \C\[End] \D2"
+    IN "\C\[Up] \C\[Down] \C\[Right] \C\[Left] \C\[Home] \C\[End] \n\D2"
 
     IN "Alt-Shift: \D1"
-    IN "\A\S\[Up] \A\S\[Down] \A\S\[Right] \A\S\[Left] \A\S\[Home] \A\S\[End] \D2"
+    IN "\A\S\[Up] \A\S\[Down] \A\S\[Right] \A\S\[Left] \A\S\[Home] \A\S\[End] \n\D2"
 
     IN "Control-Shift: \D1"
-    IN "\C\S\[Up] \C\S\[Down] \C\S\[Right] \C\S\[Left] \C\S\[Home] \C\S\[End] \D2"
+    IN "\C\S\[Up] \C\S\[Down] \C\S\[Right] \C\S\[Left] \C\S\[Home] \C\S\[End] \n\D2"
 
     IN "Control-Alt: \D1"
-    IN "\C\A\[Up] \C\A\[Down] \C\A\[Right] \C\A\[Left] \C\A\[Home] \C\A\[End] \D2"
+    IN "\C\A\[Up] \C\A\[Down] \C\A\[Right] \C\A\[Left] \C\A\[Home] \C\A\[End] \n\D2"
 
     IN "Control-Alt-Shift: \D1"
-    IN "\C\A\S\[Up] \C\A\S\[Down] \C\A\S\[Right] \C\A\S\[Left] \C\A\S\[Home] \C\A\S\[End] \D2"
+    IN "\C\A\S\[Up] \C\A\S\[Down] \C\A\S\[Right] \C\A\S\[Left] \C\A\S\[Home] \C\A\S\[End] \n \D2"
 
-    SNAP keys_03 0721436bc76609f83491fbb652045c5d
+    SNAP keys_03 f1a4d62a34b3a9cbbc79369eb320ff4e
 
     IN "\Cd\Cd\D3"
+    IN "printf \"\\\\e[H\\\\e[J\\\\e[?1h\" && cat -vT\r"
+
+    IN "Testing cursor keys in application mode (DECAKM).\n\D1"
+    IN "No modifiers: \D1"
+    IN "\[Up] \[Down] \[Right] \[Left] \[Home] \[End] \n\D2"
+
+    IN "Shift: \D1"
+    IN "\S\[Up] \S\[Down] \S\[Right] \S\[Left] \S\[Home] \S\[End] \n\D2"
+
+    IN "Alt: \D1"
+    IN "\A\[Up] \A\[Down] \A\[Right] \A\[Left] \A\[Home] \A\[End] \n\D2"
+
+    IN "Control: \D1"
+    IN "\C\[Up] \C\[Down] \C\[Right] \C\[Left] \C\[Home] \C\[End] \n\D2"
+
+    IN "Alt-Shift: \D1"
+    IN "\A\S\[Up] \A\S\[Down] \A\S\[Right] \A\S\[Left] \A\S\[Home] \A\S\[End] \n\D2"
+
+    IN "Control-Shift: \D1"
+    IN "\C\S\[Up] \C\S\[Down] \C\S\[Right] \C\S\[Left] \C\S\[Home] \C\S\[End] \n\D2"
+
+    IN "Control-Alt: \D1"
+    IN "\C\A\[Up] \C\A\[Down] \C\A\[Right] \C\A\[Left] \C\A\[Home] \C\A\[End] \n\D2"
+
+    IN "Control-Alt-Shift: \D1"
+    IN "\C\A\S\[Up] \C\A\S\[Down] \C\A\S\[Right] \C\A\S\[Left] \C\A\S\[Home] \C\A\S\[End] \n \D2"
+
+    SNAP keys_04 b94f9ce3b7363c0ec559c9de338273ab
+
+    IN "\Cd\Cd\D3"
+    IN "printf \"\\\\e[?1l\"\r"
 }
 
 function KEYPAD_KEYS {
     IN "printf \"\\\\e[H\\\\e[J\" && cat -vT\r"
 
-    IN "Testing keypad keys in normal mode. \n\D1"
+    IN "Testing keypad keys in normal mode (DECKPNM).\n\D1"
     IN "No modifiers: \D1"
     # VT100 Numeric mode - test both "Numlock on" and "NumLock off" keysyms
     IN "\[KP_Tab] \[KP_Space] \[KP_Multiply] \[KP_Divide] "
@@ -141,12 +172,12 @@ function KEYPAD_KEYS {
     IN "\C\A\[KP_Begin] \C\A\[KP_Right] \C\A\[KP_Home] \C\A\[KP_Up] \C\A\[KP_Page_Up] "
     IN "\C\A\[KP_Enter] \D2"
 
-    SNAP keys_04 bb57a78dfa5d5b85ca9714ec5ddbf6cf
+    SNAP keys_05 cd54062d5b291a30fd09bea0fb122e86
 
     IN "\Cd\Cd\D3"
     IN "printf \"\\\\e[H\\\\e[J\\\\e=\" && cat -vT\r"
 
-    IN "Testing keypad keys in application keypad mode. \n\D1"
+    IN "Testing keypad keys in application keypad mode (DECKPAM).\n\D1"
     IN "No modifiers: \D1"
     # VT100 Application mode - test both "Numlock on" and "NumLock off" keysyms
     IN "\[KP_Tab] \[KP_Space] \[KP_Multiply] \[KP_Divide] "
@@ -184,7 +215,7 @@ function KEYPAD_KEYS {
     IN "\C\A\[KP_Begin] \C\A\[KP_Right] \C\A\[KP_Home] \C\A\[KP_Up] \C\A\[KP_Page_Up] "
     IN "\C\A\[KP_Enter]    \D2"
 
-    SNAP keys_05 d54bdfb3f3b5773697b2cf3b7f8af1d1
+    SNAP keys_06 fb590f65c41ca5cf5b49bf17d366059b
 
     IN "\Cd\Cd\D3"
     IN "printf \"\\\\e>\"\r"
@@ -199,7 +230,7 @@ function ALT_SENDS_ESC {
     IN "\A0 \A1 \A2 \A3 \A4 \A5 \A6 \A7 \A8 \A9 "
     IN "\C\A2 \C\A3 \C\A5 \C\A6 \C\A7 "
 
-    SNAP keys_06 fa78d452ab0fde532cc24ffcd8bbbf7f
+    SNAP keys_07 fa78d452ab0fde532cc24ffcd8bbbf7f
 
     IN "\Cd\Cd\D3"
     IN "stty -echo && printf \"\\\\e[H\\\\e[J\\\\e[12l\" && cat -vT\r"
@@ -210,7 +241,7 @@ function ALT_SENDS_ESC {
     IN "\A0 \A1 \A2 \A3 \A4 \A5 \A6 \A7 \A8 \A9 "
     IN "\C\A2 \C\A3 \C\A5 \C\A6 \C\A7 "
 
-    SNAP keys_07 67051d652516e3a0e2198b9e28219c25
+    SNAP keys_08 67051d652516e3a0e2198b9e28219c25
 
     IN "\Cd\Cd\D3"
     IN "printf \"\\\\e[12h\" && stty echo\r"
@@ -229,7 +260,7 @@ function ALT_SENDS_ESC {
     IN "\C\Aa \C\AA \C\A0 \C\A1 \C\A2 \C\A9 \A# \C\A# "
     IN "\C\A/ \C\A? "
 
-    SNAP keys_08 0b532193433fb81966582cc255fa18a8
+    SNAP keys_09 0b532193433fb81966582cc255fa18a8
 
     IN "\Cd\Cd\D3"
     IN "printf \"\\\\e[?1036h\"\r"
@@ -329,7 +360,7 @@ function COMPOSE_CHARS {
     IN " \[Multi_key]!!"
     IN " \[Multi_key]??"
 
-    SNAP keys_09 60554ad0c7ff32768f1536016cafde4d
+    SNAP keys_10 60554ad0c7ff32768f1536016cafde4d
 
     IN "\Cd\Cd\D3"
 }
@@ -383,7 +414,7 @@ function SPECIAL_KEYS {
     IN "Alt-Tab: \A\[Tab] \D2"
     IN "Alt-x: \Ax \D2"
 
-    SNAP keys_10 a415b0df0b027d5bb9b66fe6e6902574
+    SNAP keys_11 a415b0df0b027d5bb9b66fe6e6902574
 
     IN "\Cd\Cd\D3"
 
@@ -437,7 +468,7 @@ function SPECIAL_KEYS {
     IN "Alt-Tab: \A\[Tab] \D2"
     IN "Alt-x: \Ax \D2"
 
-    SNAP keys_11 dc0833c6fcfc7a5f6668597158502345
+    SNAP keys_12 dc0833c6fcfc7a5f6668597158502345
 
     IN "\Cd\Cd\D3"
 
@@ -494,7 +525,7 @@ function SPECIAL_KEYS {
     IN "alt-tab \A\[Tab] \D2"
     IN "alt-x \Ax \D2"
 
-    SNAP keys_12 ceb5fe868904bc8f716254a0ce6f3807
+    SNAP keys_13 ceb5fe868904bc8f716254a0ce6f3807
 
     IN "\Cd\Cd\D3"
 }